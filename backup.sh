0cec18264627b5cd65c618f19f2fc434aeff5f7c
Lei Chen <leochen4891@gmail.com>
2015-07-21 11:40:40 -0400

    20150721

--- backup.sh
+++ backup.sh
@@ -10,3 +10,16 @@ sudo ln -P hbase-server.jar         hbase-server.jar.bak
 sudo ln -P hbase-shell.jar          hbase-shell.jar.bak
 sudo ln -P hbase-testing-util.jar   hbase-testing-util.jar.bak
 sudo ln -P hbase-thrift.jar         hbase-thrift.jar.bak
+
+sudo rm -f hbase-client.jar        
+sudo rm -f hbase-common.jar        
+sudo rm -f hbase-examples.jar      
+sudo rm -f hbase-hadoop2-compat.jar
+sudo rm -f hbase-hadoop-compat.jar 
+sudo rm -f hbase-it.jar            
+sudo rm -f hbase-prefix-tree.jar   
+sudo rm -f hbase-protocol.jar      
+sudo rm -f hbase-server.jar        
+sudo rm -f hbase-shell.jar         
+sudo rm -f hbase-testing-util.jar  
+sudo rm -f hbase-thrift.jar        
