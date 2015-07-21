c5220a3b395f7101bd3b3b7f7db4df14793499f7
leochen4891 <leochen4891@gmail.com>
2015-07-21 15:46:19 -0400

    20150721

--- jars/collect_jars.sh
+++ jars/collect_jars.sh
@@ -2,6 +2,19 @@ HBASE_HOME="/home/leochen4891/github/hbase"
 
 rm *.jar
 
+ls -alF $HBASE_HOME/hbase-client/target/hbase-client-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-common/target/hbase-common-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-examples/target/hbase-examples-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-hadoop2-compat/target/hbase-hadoop2-compat-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-hadoop-compat/target/hbase-hadoop-compat-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-it/target/hbase-it-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-prefix-tree/target/hbase-prefix-tree-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-protocol/target/hbase-protocol-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-server/target/hbase-server-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-shell/target/hbase-shell-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-testing-util/target/hbase-testing-util-2.0.0-SNAPSHOT.jar
+ls -alF $HBASE_HOME/hbase-thrift/target/hbase-thrift-2.0.0-SNAPSHOT.jar
+
 cp $HBASE_HOME/hbase-client/target/hbase-client-2.0.0-SNAPSHOT.jar hbase-client.jar
 cp $HBASE_HOME/hbase-common/target/hbase-common-2.0.0-SNAPSHOT.jar hbase-common.jar
 cp $HBASE_HOME/hbase-examples/target/hbase-examples-2.0.0-SNAPSHOT.jar hbase-examples.jar
