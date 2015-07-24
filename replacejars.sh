bae312a1b03204b1a27ee77df69af624c7f3375f
Lei Chen <lchen403@bloomberg.net>
2015-07-24 10:36:11 -0400

    20150724

--- replacejars.sh
+++ replacejars.sh
@@ -1,5 +1,5 @@
 HBASE_LIB="/usr/hdp/2.2.0.0-2041/hbase/lib"
-SOURCE_JAR_PATH="ubuntu@100.80.16.77:/home/ubuntu/github/swap/jars"
+SOURCE_JAR_PATH="ubuntu@bcpc-vm1:/home/ubuntu/github/swap/jars"
 JAR_FILES="
 hbase-checkstyle.jar
 hbase-server.jar
@@ -22,7 +22,7 @@ hbase-thrift.jar"
 
 for file in $JAR_FILES
 do
-	sudo cp -l $HBASE_LIB/$file $HBASE_LIB/$file.bak
+	sudo cp -ln $HBASE_LIB/$file $HBASE_LIB/$file.bak
 	sudo rm -f $HBASE_LIB/$file
 done
 
